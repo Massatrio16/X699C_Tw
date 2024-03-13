bfccc0db360da797af897847bdb40b07c3fe4bfc
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-03-13 10:14:19 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,7 @@ DEVICE_PATH := device/infinix/X699C
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
-
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
